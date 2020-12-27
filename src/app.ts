4e0c41a021ac0c933215d17b035222925fcef55b
IbnuAhsani <ibnu.muhari@gmail.com>
2020-12-27 10:36:58 +0700

    build: install & setup prettier config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
+  res.send('hello world');
+});
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
